84f2518ba6efe83f40acf32e4906b4a5b07d893f
imprakhar <31896710+imprakhar@users.noreply.github.com>
2019-09-25 13:10:35 +0530

    Final SpringWebMvc

---
+++ src/main/java/wp/springmvc/BookDAOInterface.java
@@ -0,0 +1,11 @@
+package wp.springmvc;
+
+import java.util.List;
+
+public interface BookDAOInterface {
+	public void insertBook(BookModel book);
+	public void deleteBook(String code);
+	public void updateBook(BookModel book);
+	public BookModel getBookById(String code);
+	public List<BookModel> getAllBooks();
+}
